f164065f729dbbf4b478922522565e65161de0a8
Thenlie <leithen113@gmail.com>
2024-09-21 19:01:34 -0600

    export getPathFromTitle function

--- src/titles/index.ts
+++ src/titles/index.ts
@@ -1,5 +1,6 @@
 import { javascriptTitles } from './js_titles.js';
 import { htmlTitles } from './html_titles.js';
 import { cssTitles } from './css_titles.js';
+import { getPathFromTitle } from './utils.js';
 
-export { javascriptTitles, htmlTitles, cssTitles };
+export { javascriptTitles, htmlTitles, cssTitles, getPathFromTitle };
